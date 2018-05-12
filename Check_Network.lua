bae55b1a3212e8635ef82b8ddb1dce96d3e52e53
AmirKaLan <32092233+CerNerCompany@users.noreply.github.com>
2018-05-12 21:49:05 +0430

    v 1.04

---
+++ Check_Network.lua
@@ -0,0 +1,15 @@
+
+checkfile = io.open("~/.conky/TestProject","r")
+if checkfile then
+os.execute("mv TestProject TestProject-backup")
+ead = io.popen("ip route get 8.8.8.8")
+local riead = ead:read("*a") 
+sub = string.gsub(riead,'(.*)dev ', '')
+sub_2 = string.gsub(sub,' src(.*)', '')
+local type,ok = io.open("TestProject",'r'):read('*a'):gsub("NETWORKTYPE",sub_2)
+io.open("ConkyQA",'w'):write(type):close()
+ead = io.popen("sudo vnstat -u -i "..sub_2)
+print('network interfaces : '..sub_2)
+else
+print' Error 404'
+end
