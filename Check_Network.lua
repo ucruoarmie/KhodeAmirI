736c1e7bf4755722ba44c0625a689da3bba5513e
AmirKaLan <32092233+CerNerCompany@users.noreply.github.com>
2018-05-13 21:54:10 +0430

    Update Check_Network.lua

--- Check_Network.lua
+++ Check_Network.lua
@@ -20,6 +20,5 @@ io.open("ConkyQA",'w'):write(type):close()
 ead = io.popen("sudo vnstat -u -i "..sub_2)
 print('Info: -> A new database has been created')
 print('network interfaces : '..sub_2)
-os.execute("sudo rm -rf TestProject")
 print('Config successfully Completed !')
 end
