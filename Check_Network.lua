6b542b4092ae3d69a8dcd563dbaf92a6bb195d63
AmirKaLan <32092233+CerNerCompany@users.noreply.github.com>
2018-05-15 01:17:24 +0430

    Update Check_Network.lua

--- Check_Network.lua
+++ Check_Network.lua
@@ -1,4 +1,3 @@
-
 checkfile = io.open("~/.conky/QPL/ConkyQA","r")
 if checkfile then
 os.execute("mv ConkyQA ConkyQA-backup")
@@ -10,6 +9,18 @@ local type,ok = io.open("TestProject",'r'):read('*a'):gsub("NETWORKTYPE",sub_2)
 io.open("ConkyQA",'w'):write(type):close()
 ead = io.popen("sudo vnstat -u -i "..sub_2)
 print('network interfaces : '..sub_2)
+checkfile_2 = io.open("~/.conky/QPL/TestProject-v2","r")
+elseif checkfile_2 then
+ead = io.popen("ip route get 8.8.8.8")
+local riead = ead:read("*a") 
+sub = string.gsub(riead,'(.*)dev ', '')
+sub_2 = string.gsub(sub,' src(.*)', '')
+local type,ok = io.open("TestProject-v2",'r'):read('*a'):gsub("NETWORKTYPE",sub_2)
+io.open("ConkyQA",'w'):write(type):close()
+ead = io.popen("sudo vnstat -u -i "..sub_2)
+print('Info: -> A new database has been created')
+print('network interfaces : '..sub_2)
+print('Config successfully Completed !')
 else
 ead = io.popen("ip route get 8.8.8.8")
 local riead = ead:read("*a") 
