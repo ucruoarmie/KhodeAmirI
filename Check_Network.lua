c184d0cc2943153da7c8d05dadf6da5bb6a5c4c3
AmirKaLan <32092233+CerNerCompany@users.noreply.github.com>
2018-05-15 17:30:33 +0430

    Update Check_Network.lua

--- Check_Network.lua
+++ Check_Network.lua
@@ -1,35 +1,22 @@
-checkfile = io.open("~/.conky/QPL/ConkyQA","r")
-if checkfile then
-os.execute("mv ConkyQA ConkyQA-backup")
+checkfile_1 = io.open("~/.conky/QPL/TestProject","r")
+checkfile_2 = io.open("~/.conky/QPL/TestProject-v2","r")
+if checkfile_1 and checkfile_2  then
 ead = io.popen("ip route get 8.8.8.8")
 local riead = ead:read("*a") 
 sub = string.gsub(riead,'(.*)dev ', '')
 sub_2 = string.gsub(sub,' src(.*)', '')
-local type,ok = io.open("TestProject",'r'):read('*a'):gsub("NETWORKTYPE",sub_2)
+-------------------------------------
+local type= io.open("TestProject",'r'):read('*a'):gsub("NETWORKTYPE",sub_2)
 io.open("ConkyQA",'w'):write(type):close()
+------------------------------------
+local type = io.open("TestProject-v2",'r'):read('*a'):gsub("NETWORKTYPE",sub_2)
+io.open("ConkyQA-V2",'w'):write(type):close()
+------------------------------------------
 ead = io.popen("sudo vnstat -u -i "..sub_2)
 print('network interfaces : '..sub_2)
-checkfile_2 = io.open("~/.conky/QPL/TestProject-v2","r")
-elseif checkfile_2 then
-ead = io.popen("ip route get 8.8.8.8")
-local riead = ead:read("*a") 
-sub = string.gsub(riead,'(.*)dev ', '')
-sub_2 = string.gsub(sub,' src(.*)', '')
-local type,ok = io.open("TestProject-v2",'r'):read('*a'):gsub("NETWORKTYPE",sub_2)
-io.open("ConkyQA",'w'):write(type):close()
-ead = io.popen("sudo vnstat -u -i "..sub_2)
 print('Info: -> A new database has been created')
 print('network interfaces : '..sub_2)
 print('Config successfully Completed !')
 else
-ead = io.popen("ip route get 8.8.8.8")
-local riead = ead:read("*a") 
-sub = string.gsub(riead,'(.*)dev ', '')
-sub_2 = string.gsub(sub,' src(.*)', '')
-local type,ok = io.open("TestProject",'r'):read('*a'):gsub("NETWORKTYPE",sub_2)
-io.open("ConkyQA",'w'):write(type):close()
-ead = io.popen("sudo vnstat -u -i "..sub_2)
-print('Info: -> A new database has been created')
-print('network interfaces : '..sub_2)
-print('Config successfully Completed !')
+print'Files Notfound Please Again Configing...'
 end
