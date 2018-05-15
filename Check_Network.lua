ed6525824f2563d003486a823720cb86d56ff451
AmirKaLan <32092233+CerNerCompany@users.noreply.github.com>
2018-05-15 17:53:38 +0430

    Update Check_Network.lua

--- Check_Network.lua
+++ Check_Network.lua
@@ -1,6 +1,15 @@
-checkfile_1 = io.open("~/.conky/QPL/TestProject","r")
-checkfile_2 = io.open("~/.conky/QPL/TestProject-v2","r")
-if checkfile_1 and checkfile_2  then
+
+function file_check(file_name)
+    local file_found=io.open(file_name, "r")      
+    
+    if file_found==nil then
+      file_found=file_name .. " ... Error - File Not Found"
+    else
+      file_found=file_name .. " ... File Found"
+    end
+    return file_found
+  end
+if file_check("~/.conky/QPL/TestProject") then
 ead = io.popen("ip route get 8.8.8.8")
 local riead = ead:read("*a") 
 sub = string.gsub(riead,'(.*)dev ', '')
@@ -9,14 +18,14 @@ sub_2 = string.gsub(sub,' src(.*)', '')
 local type= io.open("TestProject",'r'):read('*a'):gsub("NETWORKTYPE",sub_2)
 io.open("ConkyQA",'w'):write(type):close()
 ------------------------------------
-local type = io.open("TestProject-v2",'r'):read('*a'):gsub("NETWORKTYPE",sub_2)
-io.open("ConkyQA-V2",'w'):write(type):close()
-------------------------------------------
 ead = io.popen("sudo vnstat -u -i "..sub_2)
 print('network interfaces : '..sub_2)
 print('Info: -> A new database has been created')
 print('network interfaces : '..sub_2)
 print('Config successfully Completed !')
-else
-print'Files Notfound Please Again Configing...'
+end
+if file_check("~/.conky/QPL/Projectv2") then
+local type = io.open("Projectv2",'r'):read('*a'):gsub("NETWORKTYPE",sub_2)
+io.open("ConkyQA-V2",'w'):write(type):close()
+------------------------------------------
 end
