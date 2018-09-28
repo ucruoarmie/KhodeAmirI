51e0a8dc559bfdf2ba98085bdcccb860bb7973ee
AmirKaLan <32092233+CerNerCompany@users.noreply.github.com>
2018-09-28 14:39:11 +0330

    Update Check_Network.lua

--- Check_Network.lua
+++ Check_Network.lua
@@ -1,35 +1,22 @@
-
 function file_check(file_name)
-    local file_found=io.open(file_name, "r")      
-    
-    if file_found==nil then
-      file_found=file_name .. " ... Error - File Not Found"
-    else
-      file_found=file_name .. " ... File Found"
-    end
-    return file_found
-  end
-if file_check("~/.conky/QPL/Project") then
-ead = io.popen("ip route get 8.8.8.8")
-local riead = ead:read("*a") 
-sub = string.gsub(riead,'(.*)dev ', '')
-sub_2 = string.gsub(sub,' src(.*)', '')
--------------------------------------
-local type= io.open("Project",'r'):read('*a'):gsub("NETWORKTYPE",sub_2)
-io.open("ConkyQA",'w'):write(type):close()
-------------------------------------
-ead = io.popen("sudo vnstat -u -i "..sub_2)
-print('network interfaces : '..sub_2)
-print('Info: -> A new database has been created')
-print('network interfaces : '..sub_2)
-print('Config successfully Completed !')
+file_found = io.open(file_name, "r")       
+if file_found==nil then
+file_found=file_name .. " ... Error - File Not Found"
+else
+file_found=file_name .. " ... File Found"
 end
-if file_check("~/.conky/QPL/Projectv3") then
-local type = io.open("Projectv3",'r'):read('*a'):gsub("NETWORKTYPE",sub_2)
-io.open("ConkyQA-3",'w'):write(type):close()
+return file_found
 end
-if file_check("~/.conky/QPL/Projectv2") then
-local type = io.open("Projectv2",'r'):read('*a'):gsub("NETWORKTYPE",sub_2)
-io.open("ConkyQA-V2",'w'):write(type):close()
-------------------------------------------
+File = {
+    '~/.conky/QPL/.Project',
+    '~/.conky/QPL/.Project2',
+    '~/.conky/QPL/.Projectv3'
+}
+ead = io.popen("ip route get 8.8.8.8");riead = ead:read("*a");sub = string.gsub(riead,'(.*)dev ', '');sub_2 = string.gsub(sub,' src(.*)', '')
+for k,v in pairs(Files) do
+if file_check(v) then
+type= io.open(v,'r'):read('*a'):gsub("NETWORKTYPE",sub_2)
+io.open("ConkyQA"..k,'w'):write(type):close()
 end
+end
+ead = io.popen("sudo vnstat -u -i "..sub_2);print('network interfaces : '..sub_2);print('Info: -> A new database has been created');print('network interfaces : '..sub_2);print('Config successfully Completed !')
